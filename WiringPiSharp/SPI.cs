532ae3a7855a595e7b951245808e9931ebb95373
MONKCs studios <ape@monkcs.se>
2016-03-24 15:32:46 +0100

    Updated SPI

--- WiringPiSharp/SPI.cs
+++ WiringPiSharp/SPI.cs
@@ -1,4 +1,4 @@
-﻿﻿// Copyright (c) TRONSoft, Inc. All rights reserved. See License.txt in the project root for license information.
+﻿// Copyright (c) TRONSoft, Inc. All rights reserved. See License.txt in the project root for license information.
 
 using System.Runtime.InteropServices;
 
@@ -10,9 +10,12 @@ namespace WiringPiSharp
         public static extern int WiringPiPiSPIGetFd(int channel);
 
         [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPIDataRW")]
-        public static extern int WiringPiPiSPIDataRW(int channel, HandleRef handle);
+		public static extern int WiringPiPiSPIDataRW(int channel, byte data, int lenght);
 
         [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPISetup")]
         public static extern int WiringPiPiSPISetup(int channel, int speed);
+		public static void test(){
+			WiringPiPiSPIDataRW (0, 0, 1);
+		}
     }
 }
