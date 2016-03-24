78582294c5050fe2941455dca2ea1d81e0de94a3
MONKCs studios <ape@monkcs.se>
2016-03-24 16:33:20 +0100

    Fixed Userprefs

--- WiringPiSharp/SPI.cs
+++ WiringPiSharp/SPI.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) TRONSoft, Inc. All rights reserved. See License.txt in the project root for license information.
+﻿﻿// Copyright (c) TRONSoft, Inc. All rights reserved. See License.txt in the project root for license information.
 
 using System.Runtime.InteropServices;
 
@@ -10,12 +10,9 @@ namespace WiringPiSharp
         public static extern int WiringPiPiSPIGetFd(int channel);
 
         [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPIDataRW")]
-		public static extern int WiringPiPiSPIDataRW(int channel, byte data, int lenght);
+        public static extern int WiringPiPiSPIDataRW(int channel, HandleRef handle);
 
         [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPISetup")]
         public static extern int WiringPiPiSPISetup(int channel, int speed);
-		public static void test(){
-			WiringPiPiSPIDataRW (0, 0, 1);
-		}
     }
 }
