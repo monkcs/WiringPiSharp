62ca0023f623c48c0772875b5444eda12b0e30c6
MONKCs studios <ape@monkcs.se>
2016-09-08 11:28:17 +0200

    Added comments to soft tone class

--- WiringPiSharp/SoftTone.cs
+++ WiringPiSharp/SoftTone.cs
@@ -6,10 +6,21 @@ namespace WiringPiSharp
 {
     public static class SoftTone
     {
+        /// <summary>
+        /// Setup the mode of a pin to software created square wave signal
+        /// </summary>
+        /// <param name="pin">The pin to setup</param>
+        public static void Create(WiringPi.WPiPinout pin) { SoftToneCreateRaw((int)pin); }
         [DllImport("libwiringPi.so", EntryPoint = "softToneCreate")]
-        public static extern int SoftToneCreate(int pi);
+        static extern int SoftToneCreateRaw(int pi);
 
+        /// <summary>
+        /// Update the tone frequency value on the given pin. The tone will be played until you set the frequency to 0.
+        /// </summary>
+        /// <param name="pin">The pin to setup</param>
+        /// <param name="frequency">The tone frequency</param>
+        public static void Write(WiringPi.WPiPinout pin, int frequency) { SoftToneWriteRaw((int)pin, frequency); }
         [DllImport("libwiringPi.so", EntryPoint = "softToneWrite")]
-        public static extern void SoftToneWrite(int pin, int frewq);
+        static extern void SoftToneWriteRaw(int pin, int frewq);
     }
 }
